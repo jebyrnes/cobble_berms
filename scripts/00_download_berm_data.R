939b01be4c6fcb46e486cf93d4fe6734ad5fcf94
jebyrnes <jarrett.byrnes@umb.edu>
2025-01-09 10:54:53 -0500

    Adding outline for downloading data

--- scripts/00_download_berm_data.R
+++ scripts/00_download_berm_data.R
@@ -1,3 +1,34 @@
 #' -------------------------------------
 #' Download data files from the google drive
 #' -------------------------------------
+
+# load libraries for accessing google drive
+# writing out CSVs
+# and doing some light data cleaning
+pacman::p_load(googlesheets4,
+               readr,
+               dplyr)
+
+###
+# Species lists
+###
+
+###
+# Quad Data
+###
+
+###
+# Seine Data
+###
+
+###
+# Trap Data
+###
+
+###
+# BRIV Data
+###
+
+###
+# FARM Data
+###
