fedf641fd43f4300ac8f7d802af84f6d1bfc43b2
crustis-r-morris <curtis.morris001@umb.edu>
2025-01-09 11:19:03 -0500

    Cleaned up quad data.

--- scripts/00_download_berm_data.R
+++ scripts/00_download_berm_data.R
@@ -8,7 +8,8 @@
 pacman::p_load(googlesheets4,
                readr,
                dplyr,
-               janitor)
+               janitor,
+               tidyr)
 
 # authorize
 gs4_auth()
@@ -23,12 +24,19 @@ gs4_auth()
 quads <- read_sheet("https://docs.google.com/spreadsheets/d/1eOic8zhjAKyBWss7iENRJUrdz52BS-GOp260UR6Pnno/edit?gid=0#gid=0",
                     sheet = "data")
 quads <- quads |>
-  remove_empty("cols")
+  remove_empty("cols") |> 
+  clean_names() |> 
+  separate_wider_regex(cols = site, 
+                       patterns = c(site = ".*", "_", 
+                                    treatment = ".*")) |> 
+  mutate(species_code = ifelse(is.na(species_code), "NOSP", species_code))
 
 visdat::vis_dat(quads)
 skimr::skim(quads)
 
 write_csv(quads, "data/quads.csv")
+
+
 
 ###
 # Seine Data
