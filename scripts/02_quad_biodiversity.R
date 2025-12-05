984704ea05b802a6df0c6c728d77a14954ad7c13
crustis-r-morris <curtis.morris001@umb.edu>
2025-12-05 06:24:10 -0500

    Worked on diversity analysis

--- scripts/02_quad_biodiversity.R
+++ scripts/02_quad_biodiversity.R
@@ -1,89 +1,125 @@
 #' -------------------------------------
-#' Look at shannon diversity
+#' Look at quadrat level richness, shannon diversity, and eveness
 #' across berms versus controls and
-#' quads %, quads #, seines, traps, and brivs 
-#' separately
+#' for percent cover and count seperatly
 #' -------------------------------------
 source("scripts/helpers.R")
 
-## load libraries
+##
+# Set-up
+##
+
+# load libraries
 library(tidyverse)
-library(broom)
+library(easystats)
+# library(broom)
 library(vegan)
-library(patchwork)
-library(emmeans)
+# library(patchwork)
+# library(emmeans)
 
-## load data
+# load data
 quad_s23_dat <- read_csv("data/quads_s23.csv")
+quad_f23_dat <- read_csv("data/quads_f23.csv")
+quad_s24_dat <- read_csv("data/quads_s24.csv")
+quad_s25_dat <- read_csv("data/quads_s25.csv")
 
-## Planning
-#' Process all the below into this new rproject(its unedited since I was working w/o git)
-#' Give it essentially the same treatment as the quads (i.e. dont double count lower levels)
-#' Calc div by taxonomic levels
-#' Analyze the same as richness, essentially ttest
+# Bind together
+quad_dat <- rbind(quad_s23_dat,
+                  quad_f23_dat,
+                  quad_s24_dat,
+                  quad_s25_dat)
+
+# Drop data that are no longer needed
+rm(quad_s23_dat)
+rm(quad_f23_dat)
+rm(quad_s24_dat)
+rm(quad_s25_dat)
+
+## Verify correct amount of quads done
+# quad_dat |> 
+#   group_by(site, treatment, season, height) |> 
+#   reframe(quad_count = n_distinct(quadrat)) |> 
+#   View()
+  
+# Problem data: 
+# Coughlin-Control-fall_23-High-2 quads only
+# Coughlin-Control-fall_23-Mid-4
+# Bayside-Berm-summer_25-High-5
+# Coughlin-Berm-summer_25-Low-5
 
 
-# Sum across squares for percent data
-percent_dat <- quad_s23_dat |> 
-  filter(measurement_type != "Count") |> 
-  group_by(site, treatment, height, quadrat, measurement_type, species_code) |> 
+##
+# Calculate Richness, biodiversity, eveness
+##
+
+# Sum to the quad level
+quad_dat <- quad_dat |> 
+  group_by(season, site, treatment, height, quadrat, measurement_type, species_code) |> 
   reframe(sum = sum(measurement)) |> 
   filter(species_code != "NOSP") |> 
+  filter(species_code != "none_present") |> 
   ungroup()
 
-# Sum across squares for count data
-count_dat <- quad_s23_dat |> 
-  filter(measurement_type == "Count") |> 
-  group_by(site, treatment, height, quadrat, measurement_type, species_code) |> 
-  reframe(sum = sum(measurement)) |> 
-  filter(species_code != "NOSP") |> 
-  ungroup()
+# Pull out quad richness w/o measurement_type
+quad_richness <- quad_dat |> 
+  group_by(season, site, treatment, height, quadrat) |> 
+  reframe(richness = n_distinct(species_code))
 
 # Pivot data for use in vegan::diversity() 
-percent_dat <- percent_dat |> 
+quad_dat <- quad_dat |> 
   pivot_wider(names_from = "species_code",
               values_from = "sum",
               values_fill = 0)
 
-count_dat <- count_dat |> 
-  pivot_wider(names_from = "species_code",
-              values_from = "sum",
-              values_fill = 0)
+# Calculate Shannon, InvSimpson, richness, evenness, and rearrange
+quad_dat <- quad_dat |> 
+  mutate(shannon = diversity(quad_dat[,-c(1:6)], index = "shannon")) # measures the uncertainty in predicting the species identity of an individual chosen at random from the community. A higher value indicates higher diversity (more species and/or more even distribution), and a lower value indicates lower diversity. It equals 0 only when there is no uncertainty—meaning there is only ONE species present.
 
-# Calc Diversity Indicies
+quad_dat <- quad_dat |> 
+  mutate(invsimpson = diversity(quad_dat[,-c(1:6,85)], index = "invsimpson")) # The Inverse Simpson Index is an alternative diversity measure that focuses more on the dominance of species. The lower the value, the greater the diversity. Unlike the Shannon-Wiener index, which treats all species equally, the Inverse Simpson Index weighs more heavily on rare species, but it gives more weight to dominant species.
 
-# Shannon and InvSimpson
-percent_dat <- percent_dat |> 
-  mutate(shannon = diversity(percent_dat[,-c(1:5)], index = "shannon")) |> 
-  mutate(invsimpson = diversity(percent_dat[,-c(1:5)], index = "invsimpson"))
+quad_dat <- quad_dat |> mutate(richness = specnumber(quad_dat[,-c(1:6),85,86])) |> 
+  mutate(evenness = shannon/log(richness))  # Pielous eveness how evenly individuals are distributed across the different species in a community. It ranges from 0 (completely uneven) to 1 (perfectly even).
+  
+quad_dat <- quad_dat |> 
+select(richness, shannon, evenness, invsimpson, everything())
+  
+## Something is wrong here. Its detecting richness of 2 when there is only a single species present
 
-count_dat <- count_dat |> 
-  mutate(shannon = diversity(count_dat[,-c(1:5)], index = "shannon")) |> 
-  mutate(invsimpson = diversity(count_dat[,-c(1:5)], index = "invsimpson"))
+  
+##
+# Visualize
+##
 
-# Hurdle Mod Analysis
+# Set up data for plotting
+div_plot_dat <- quad_dat[,-c(11:88)] |> 
+  pivot_longer(cols = richness:invsimpson, 
+               values_to = "value", 
+               names_to = "index")
 
-# Odds of being over 0
-percent_dat_hurdle_mod <- glm(shannon > 0 ~ treatment + height + site,
-                             family = binomial(link = "logit"),
-                             data = percent_dat)
 
-count_dat_hurdle_mod <- glm(shannon > 0 ~ treatment + height + site,
-                             family = binomial(link = "logit"),
-                             data = count_dat)
+ggplot(div_plot_dat,
+       aes(x = treatment,
+           y = value)) +
+  geom_boxplot() +
+  facet_grid(rows = vars(height), cols = vars(index))
 
-# Model for those over 0
-percent_dat_mod <- lm(sqrt(shannon) ~ treatment + height + site,
-                     data = percent_dat |> 
-                       filter(shannon > 0))
 
-count_dat_mod <- lm(sqrt(shannon) ~ treatment + height + site,
-                     data = count_dat |> 
-                       filter(shannon > 0))
 
-# viz
+##
+# Analysis
+##
 
-## by count
+# Models for those over 0
+percent_shannon_mod <- lm(sqrt(shannon) ~ treatment + height + site,
+                      data = quad_dat |> 
+                        filter(measurement_type != "Count"))
+
+count_shannon_mod <- lm(sqrt(shannon) ~ treatment + height + site,
+                    data = quad_dat |> 
+                      filter(measurement_type == "Count"))
+
+
 
 jitter_width <- 0.1
 jitter_positions <- position_dodge(width = jitter_width)
@@ -142,6 +178,40 @@ shannon_plot <- shannon_count_plot + shannon_cover_plot +
 ggsave("figures/shannon_plot.jpg", 
        shannon_plot,
        dpi = 600)
+
+
+
+
+
+
+
+
+##
+# Analysis
+##
+
+
+# Hurdle Mod Analysis
+
+# Odds of being over 0
+percent_dat_hurdle_mod <- glm(shannon > 0 ~ treatment + height + site,
+                             family = binomial(link = "logit"),
+                             data = percent_dat)
+
+count_dat_hurdle_mod <- glm(shannon > 0 ~ treatment + height + site,
+                             family = binomial(link = "logit"),
+                             data = count_dat)
+
+# Model for those over 0
+percent_dat_mod <- lm(sqrt(shannon) ~ treatment + height + site,
+                     data = percent_dat |> 
+                       filter(shannon > 0))
+
+count_dat_mod <- lm(sqrt(shannon) ~ treatment + height + site,
+                     data = count_dat |> 
+                       filter(shannon > 0))
+
+
 
 # Get model stats
 
